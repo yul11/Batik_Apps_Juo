b4e1115230e97e59fa4d8a39452c648b6b510091
yul_in <yul_in@yul-PC.fritz.box>
2018-12-16 19:52:25 +0100

    Added first release of second-hand.

--- BatikApps_juo/src/batik/apps/juo/LineMovement.java
+++ BatikApps_juo/src/batik/apps/juo/LineMovement.java
@@ -27,9 +27,7 @@ public class LineMovement implements Runnable{
 	public void run(){	
 		
 		while (!Thread.currentThread().isInterrupted()) {
-							
-			System.out.println("LineMovement-thread laeuft...");
-			
+									
 			try {
 				Thread.sleep(100);
 			} 
@@ -37,29 +35,62 @@ public class LineMovement implements Runnable{
 				e.printStackTrace();
 			}
 							
-			canvas.getUpdateManager().getUpdateRunnableQueue().invokeLater(new Runnable() {
-				public void run(){													
+			//canvas.getUpdateManager().getUpdateRunnableQueue().invokeLater(new Runnable() {
+				//public void run(){													
 					
 					System.out.println("LineMovement()-> bin in invokeLater line");	
 					
-					Element elt = document.getElementById("theLine");
-					System.out.println("LineMovement()-> style: " + elt.getAttribute("style"));
-										
-					String str_x2= elt.getAttribute("x2");
-					System.out.println("str_x2: " + str_x2);										
-					int xPos = Integer.parseInt(elt.getAttribute("x2"));					
-					xPos = xPos +10;					
-					elt.setAttribute("x2", "" + xPos);
-
-					String str_y2= elt.getAttribute("y2");
-					System.out.println("str_y2: " + str_y2);										
-					int yPos = Integer.parseInt(elt.getAttribute("y2"));					
-					yPos = yPos -10;					
-					elt.setAttribute("y2", "" + yPos);
+				
+					int sec=0;
 					
+					while (true){
+					
+						
+						Element elt = document.getElementById("theLine");
+						System.out.println("LineMovement()-> style: " + elt.getAttribute("style"));
+
+						
+						double alf = (90 - 6 * sec) * Math.PI / 180;
+						double x = Math.cos(alf) *190;
+						double y = Math.sin(alf) *190;
+						
+						System.out.println("LineMovement()-> sec: " + sec);
+						
+						int int_x = (int)x;
+						int int_y = (int)y;
+						System.out.println("LineMovement()-> int_x: " + int_x);
+						System.out.println("LineMovement()-> int_y: " + int_y + "\n");
+						
+
+						
+						elt.setAttributeNS(null,"x1", "300");
+						elt.setAttributeNS(null,"y1", "200");
+						elt.setAttributeNS(null,"x2", Integer.toString(int_x+300));
+						elt.setAttributeNS(null,"y2", Integer.toString(int_y+200));
+						
+						
+						System.out.println("x1()-> : 300");
+						System.out.println("y1()-> : 200");
+						System.out.println("x2()-> : " + Integer.toString(int_x+300));
+						System.out.println("y2()-> : " + Integer.toString(int_y+200) + "\n");
+						
+						
+						try {
+							Thread.sleep(1000);
+						} 
+						catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						
+						sec++;
+						
+						if (sec>59){
+							sec=0;
+						}						
+					}
 				}
-			});								
+			//});								
 		}
 	}
-}
+//}
 //LINE END
