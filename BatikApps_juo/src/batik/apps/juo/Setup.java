726a21e9b6615d119137d01cb87b80138d84d7bb
yul_in <yul_in@yul-PC.fritz.box>
2018-12-30 19:03:25 +0100

    added JColorChooser()-class to setup background-color.

--- BatikApps_juo/src/batik/apps/juo/Setup.java
+++ BatikApps_juo/src/batik/apps/juo/Setup.java
@@ -2,13 +2,20 @@ package batik.apps.juo;
 
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.JTextField;
 import javax.swing.ListCellRenderer;
 import javax.swing.SwingConstants;
 import javax.swing.plaf.basic.BasicComboBoxRenderer;
@@ -25,6 +32,9 @@ public class Setup extends JFrame implements ActionListener{
 	public static JButton button1;
 	public static JButton button2;
 	public static JComboBox<String> comboBox;
+	public static JTextField txt_background;
+	public static JTextField txt_foreground;
+
 	private Document document; // The SVG document
 	private JSVGCanvas canvas;	
 	
@@ -51,8 +61,7 @@ public class Setup extends JFrame implements ActionListener{
 		String[] choices = {"black girl","red girl","CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6","CHOICE 7"};
 	    comboBox = new JComboBox<String>(choices);
 	    comboBox.setVisible(true);
-	    comboBox.setPreferredSize(dim);
-	    
+	    comboBox.setPreferredSize(dim);	    
 	    final String fontName = comboBox.getSelectedItem().toString();	    
 	    comboBox.setFont(new Font(fontName, Font.BOLD, 14));
 	    ListCellRenderer<? super String> renderer = comboBox.getRenderer();
@@ -60,7 +69,12 @@ public class Setup extends JFrame implements ActionListener{
 	        ((BasicComboBoxRenderer) renderer).setHorizontalAlignment(SwingConstants.CENTER);	    
 	    setSize(170, 650);
 	    setLocation(430, 100);
-	    
+	    JLabel hd_parameters = new JLabel("Parameters:");
+	    JTextField txt_background       = new JTextField("background-color");
+	    txt_background.setPreferredSize(dim);
+	    JTextField txt_foreground       = new JTextField("foreground-color");
+	    txt_foreground.setPreferredSize(dim);
+	    	    
 		JPanel    mainPanel   = new JPanel();		
 		mainPanel.setLayout(new BorderLayout());
 		JPanel northPanel = new JPanel();
@@ -68,6 +82,9 @@ public class Setup extends JFrame implements ActionListener{
 		northPanel.add(comboBox,BorderLayout.LINE_START);	
 		northPanel.add(button1,BorderLayout.LINE_START);	
 		northPanel.add(button2,BorderLayout.LINE_START);
+		northPanel.add(hd_parameters,BorderLayout.LINE_START);
+		northPanel.add(txt_background,BorderLayout.LINE_START);
+		northPanel.add(txt_foreground,BorderLayout.LINE_START);
 				
 		mainPanel.add(northPanel,BorderLayout.CENTER);				
 	    add(mainPanel);
@@ -79,11 +96,51 @@ public class Setup extends JFrame implements ActionListener{
 	    comboBox.addActionListener(this);
 	    button1.addActionListener(this);
 	    button2.addActionListener(this);
+	    txt_background.addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("txt_background wurde angeklickt");	
+				Color ausgewaehlteFarbe = JColorChooser.showDialog(null,"Farbe Hintergrund auswählen", null);
+				System.out.println("ausgewaehlteFarbe: " + ausgewaehlteFarbe);
+				System.out.println("rot: " + ausgewaehlteFarbe.getRed());
+				System.out.println("grün" + ausgewaehlteFarbe.getGreen());
+				System.out.println("blau: " + ausgewaehlteFarbe.getBlue());
+				txt_background.setText("[R,G,B] " + " ["+ ausgewaehlteFarbe.getRed() + "," + ausgewaehlteFarbe.getGreen() + "," + ausgewaehlteFarbe.getBlue() + "]");
+				String hex = String.format("#%02x%02x%02x", ausgewaehlteFarbe.getRed(), ausgewaehlteFarbe.getGreen(), ausgewaehlteFarbe.getBlue());  
+				Element el = document.getElementById("theClockFace");
+        		el.setAttributeNS(null, "fill",hex);
+			}
+			public void mouseEntered(MouseEvent arg0) {
+				System.out.println("txt_background mouse entered!!!!");	
+			}
+			public void mouseExited(MouseEvent arg0) {	
+				System.out.println("txt_background mouse exited!!!!");	
+			}
+			public void mousePressed(MouseEvent arg0) {
+				System.out.println("txt_background mousePressed!!!!");	
+			}
+			public void mouseReleased(MouseEvent arg0) {
+				System.out.println("txt_background mouseReleased!!!!");	
+			}
+	    });
+	    
+	    
+	    txt_foreground.addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("txt_foreground wurde angeklickt");				
+			}
+			public void mouseEntered(MouseEvent arg0) {
+			}
+			public void mouseExited(MouseEvent arg0) {
+			}
+			public void mousePressed(MouseEvent arg0) {
+			}
+			public void mouseReleased(MouseEvent arg0) {
+			}   	
+	    });
 	}
 
 	
-	public void actionPerformed(ActionEvent e) {
-		
+	public void actionPerformed(ActionEvent e) {		
 		if (e.getSource() == button1){
 			System.out.println("Analog-button gedrückt");
 			setVisible(true);
